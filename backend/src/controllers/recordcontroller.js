f1a7ca431b27f49c14ee8781e5f820e05e948851
ChethanRaj13 <chethanrajjee@gmail.com>
2025-12-10 19:55:45 +0530

    added create function

--- backend/src/controllers/recordcontroller.js
+++ backend/src/controllers/recordcontroller.js
@@ -1,2 +1,16 @@
 const Record = require('../models/Record')
 
+exports.createRecord = async (req, res, next) => {
+    try {
+        const { name, amount, type, date } = req.body;
+        if(!name || amount == null || !type){
+            return res.status(400).json({message: "please enter name, amount and type"});
+        }
+        const record = new Record({name, amount, type, date});
+        const save = await.record.save();
+        res.status(201).json(saved);
+    }
+    catch (err){
+        next(err);
+    }
+};
